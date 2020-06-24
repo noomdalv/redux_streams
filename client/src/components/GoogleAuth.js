927644bed39167be7e762700bc593522d772dea6
noomdalv <vladlunasan@gmail.com>
2020-06-24 01:04:11 -0500

    added GoogleAuth signIn/signOut functions for authentication with oauth

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -21,13 +21,23 @@ class GoogleAuth extends React.Component {
 		this.setState({ isSignedIn: this.auth.isSignedIn.get() });
 	}
 
+	onSignIn = () => {
+		this.auth.signIn();
+	}
+
+	onSignOut = () => {
+		this.auth.signOut();
+	}
+
 	renderAuthButton() {
 		if (this.state.isSignedIn === null) {
-			return <div>I dont know.</div>
+			return null;
 		} else if (this.state.isSignedIn) {
-			return <div>I'm signed in.</div>
+			return (
+				<button onClick={this.onSignOut} className="ui red google button"><i className="google icon" />Sign Out</button>
+			)
 		} else {
-			return <div>I'm not signed in.</div>
+			return <button onClick={this.onSignIn} className="ui green google button"><i className="google icon" />Sign In with Google</button>
 		}
 	}
 
