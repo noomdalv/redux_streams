420c512e8822b6e08c569493eb8452d3414e6323
noomdalv <vladlunasan@gmail.com>
2020-06-28 21:00:44 -0500

    added json-server for api calls at port 3001 and axios/thunk setup

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,4 @@
+import streams from '../api/streams';
 import { SIGN_IN, SIGN_OUT } from './types';
 
 export const signIn = (userId) => ({
@@ -8,3 +9,7 @@ export const signIn = (userId) => ({
 export const signOut = () => ({
 	type: SIGN_OUT
 });
+
+export const createStream = formValues => async dispatch => {
+	streams.post("/streams", formValues);
+}
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component {
 	renderError({ error, touched }) {
@@ -23,8 +25,9 @@ class StreamCreate extends React.Component {
 		);
 	}
 
-	onSubmit(formValues) {
+	onSubmit = formValues => {
 		console.log(formValues);
+		this.props.createStream(formValues)
 	}
 
 	render() {
@@ -49,4 +52,6 @@ const validate = formValues => {
 	return errors;
 };
 
-export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
+const formWrapper = reduxForm({ form: "streamCreate", validate })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapper);
