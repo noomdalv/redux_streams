987de3bc2e8de62c99253860dcdbdc5a53cf0212
noomdalv <vladlunasan@gmail.com>
2020-06-29 19:21:28 -0500

    created StreamList methods to render buttons for create and edit/delete actions

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,13 +1,64 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
+
 
 class StreamList extends React.Component {
+	componentDidMount() {
+		this.props.fetchStreams();
+	}
+
+	renderList() {
+		console.log(this.props.streams);
+		return this.props.streams.map(stream => {
+			return (
+				<div className="item" key={stream.id}>
+					{this.renderEditDelete(stream)}
+					<i className="large middle aligned icon camera" />
+					<div className="content">{stream.title}</div>
+					<div className="description">{stream.description}</div>
+				</div>
+			);
+		})
+	}
+
+	renderEditDelete(stream) {
+		if (stream.userId === this.props.currentUserId) {
+			return (
+				<div className="right floated content">
+					<button className="ui button primary">Edit</button>
+					<button className="ui button negative">Delete</button>
+				</div>
+			);
+		}
+	}
+
+	renderCreate() {
+		if (this.props.isSignedIn) {
+			return (
+				<div style={{ textAlign: "right" }}>
+					<Link to="/streams/new" className="ui button primary">Create Stream</Link>
+				</div>
+			);
+		}
+	}
+
 	render() {
 		return (
 			<div>
-				StreamList
+				<h2>Streams</h2>
+				<div className="ui celled list">{this.renderList()}</div>
+				{this.renderCreate()}
 			</div>
 		);
 	}
 };
 
-export default StreamList;
+const mapStateToProps = state => ({
+	streams: Object.values(state.streams),
+	currentUserId: state.auth.userId,
+	isSignedIn: state.auth.isSignedIn
+});
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
